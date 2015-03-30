0d94ad439b18327ff37cc121d015f339ee76006c
ginsederp <ginsederp@gmail.com>
2015-03-30 09:27:08 +0800

    Added time accessed counter.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var timesAccessed = 0;
 /* GET home page. */
 router.get('/', function(_req, _res) {
+  timesAccessed++;
+
   _res.render('index', {
-    title: 'Hello World!'
+      title: 'Hello World!'
+    , times_accessed: timesAccessed
   });
 });
 
