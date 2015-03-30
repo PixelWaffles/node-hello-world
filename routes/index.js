b0306692d2eacf825a1492aefcd3c572e1ba1a3d
ginsederp <ginsederp@gmail.com>
2015-03-30 09:41:48 +0800

    Added time tracking in console.

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 var timesAccessed = 0;
+var accessDateTime;
 /* GET home page. */
 router.get('/', function(_req, _res) {
   timesAccessed++;
+  
+  accessDateTime = new Date();
+  var accessDateTimeString = accessDateTime.toJSON();
+  
+  console.log(
+  	  "Connection ping @ :"
+  	+ accessDateTime
+  	+ ". This server has been accessed "
+  	+ timesAccessed
+  	+ " times."
+  	);
 
   _res.render('index', {
       title: 'Hello World!'
