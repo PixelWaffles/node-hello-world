668f67db66cd525e42137c3be47609496117afde
ginsederp <ginsederp@gmail.com>
2015-03-30 09:13:36 +0800

    Changed title to Hello World.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(_req, _res) {
+  _res.render('index', {
+    title: 'Hello World!'
+  });
 });
 
 module.exports = router;
